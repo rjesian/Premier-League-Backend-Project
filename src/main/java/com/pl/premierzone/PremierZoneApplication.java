c30ebfc35b436afdd020549d22ba418a26e6b275
Rifah Jesian <rifahj808@gmail.com>
2025-11-29 21:59:49 -0500

    Initial commit of Player-Teams Spring Boot project

---
+++ src/main/java/com/pl/premierzone/PremierZoneApplication.java
@@ -0,0 +1,13 @@
+package com.pl.premierzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierZoneApplication.class, args);
+	}
+
+}
